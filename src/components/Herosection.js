9e7c6751c5e7b9395885e5e3f391629767352df4
Niraj Giri <nirajgiri@Nirajs-MacBook-Air.local>
2024-01-30 20:19:19 +0545

    Fix process env:

--- src/components/Herosection.js
+++ src/components/Herosection.js
@@ -1,19 +1,26 @@
 import ConfessionList from "./ConfessionList";
 
 async function getConfession() {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/confessions/` ||
-      `${NEXT_PUBLIC_API_URL}`,
-    {
+  try {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || NEXT_PUBLIC_API_URL;
+    const res = await fetch(`${apiUrl}/api/confessions/`, {
       cache: "no-cache",
+    });
+
+    if (!res.ok) {
+      throw new Error(
+        `Failed to fetch data: ${res.status} - ${res.statusText}`
+      );
     }
-  );
 
-  if (!res.ok) {
-    throw new Error(`Failed to fetch data: ${res.status} - ${res.statusText}`);
+    return res.json();
+  } catch (error) {
+    console.error(
+      `Error fetching data from ${process.env.NEXT_PUBLIC_API_URL}:`,
+      error
+    );
+    throw error; // Re-throw the error to be caught by the higher-level catch block
   }
-
-  return res.json();
 }
 
 const Herosection = async () => {
@@ -25,7 +32,7 @@ const Herosection = async () => {
       </>
     );
   } catch (error) {
-    console.error("Error fetching data:", error);
+    console.error("Error rendering Herosection:", error);
     return null;
   }
 };
